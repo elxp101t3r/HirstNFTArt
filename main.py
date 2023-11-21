f0fc11cbc8d4d3c159c4d752b660b1a6b0915112
elxp101t3r <eliotx10101@outlook.com>
2023-11-21 20:17:08 +0200

    Damien Hirst - Loperamide

---
+++ main.py
@@ -0,0 +1,30 @@
+from turtle import Turtle
+from random import choice, randint
+import colorgram
+
+colors = colorgram.extract('hirst.png', 30)
+rgbs = []
+
+for c in colors:
+    rgbs.append((c.rgb.r, c.rgb.g, c.rgb.b))
+
+
+tim = Turtle()
+tim.shape('circle')
+tim.penup()
+y = -250
+tim.setpos(-250, y)
+
+
+def paint():
+    for draw in range(0, 10):
+        tim.fd(50)
+        tim.dot(30, (rgbs[randint(0, len(rgbs) - 1)][0]/255.0, rgbs[randint(0, len(rgbs) - 1)][1]/255.0, rgbs[randint(0, len(rgbs) - 1)][2]/255.0))
+
+
+for c in range(0,10):
+    paint()
+    y += 50
+    tim.setpos(-250, y)
+tim.hideturtle()
+tim.screen.mainloop()
